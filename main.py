fe9e80697e2eb3b19e3c26cd3e37e5278b992b22
sonarAIT <sonarmikumiku@gmail.com>
2023-06-02 23:39:57 +0900

    [feat] 電車でGO!に入力するモジュール

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import pyautogui
 
 class ZuikiControllerInputGetter:
     def __init__(self):
@@ -48,11 +49,53 @@ class ZuikiControllerInputGetter:
         notch = self.AXIS2NOTCH[axis]
         return notch
 
+class PS1DengoKeyPresser:
+    def __init__(self):
+        self.NOTCH2BITS = {
+            "P5": 0b01011011,
+            "P4": 0b11001011,
+            "P3": 0b11011011,
+            "P2": 0b01101011,
+            "P1": 0b01111011,
+            "N": 0b11101011,
+            "B1": 0b11101110,
+            "B2": 0b11101010,
+            "B3": 0b11100111,
+            "B4": 0b11100011,
+            "B5": 0b11100110,
+            "B6": 0b11100010,
+            "B7": 0b11101101,
+            "B8": 0b11101001,
+            "EB": 0b11100000,
+        }
+        self.BIT2KEY = {
+            0: "2",
+            1: "0",
+            2: "1",
+            3: "9",
+            4: "i",
+            5: "d",
+            6: "s",
+            7: "a",
+        }
+
+    def press(self, notch):
+        pyautogui.keyDown("w") # wは押しっぱなし
+        bits = self.NOTCH2BITS[notch]
+        for i in range(8):
+            if bits & (1 << i):
+                pyautogui.keyDown(self.BIT2KEY[i])
+            else:
+                pyautogui.keyUp(self.BIT2KEY[i])
+
 def main():
     controllerInputGetter = ZuikiControllerInputGetter()
+    keyPresser = PS1DengoKeyPresser()
     while True:
-        print(controllerInputGetter.getNotch())
-        pygame.time.wait(10)
+        # print(controllerInputGetter.getNotch())
+        # pygame.time.wait(10)
+        keyPresser.press()
+
 
 if __name__ == "__main__":
     main()
