7b84a5f581be69c181e13c99726bddf71ef8759b
sonarAIT <sonarmikumiku@gmail.com>
2025-01-02 01:58:05 +0900

    [fix]

--- scripts/Init.ps1
+++ scripts/Init.ps1
@@ -1,4 +1,4 @@
 python -m venv venv
 .\venv\Scripts\Activate.ps1;
-pip install --upgrade pip;
-pip install -r requirements.txt;
+python -m pip install --upgrade pip;
+python -m pip install -r requirements.txt;
